10fd6f79b6d4921aef297f793d31dfd45a8b2428
Megan Seritan <meganseritan@Megans-MacBook-Air.local>
2026-08-29 22:01:31 -0700

    Add category gallery pages with multi-column layout

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,21 +2,21 @@ export const portfolio = [
     {
         supercategoryTitle: "Dataviz",
         categories: [
-            { slug: "personal-projects", title: "Personal Projects", folder: "dataviz/personal-projects", cover: "01-nightingale-redo.png" },
-            { slug: "grad-school-projects", title: "Grad School Projects", folder: "dataviz/grad-school-projects", cover: "01-geologic-map.png" },
-            { slug: "sports-viz-sunday", title: "Sports Viz Sunday", folder: "dataviz/sports-viz-sunday", cover: "01-wheelchair-marathon.png" },
+            { slug: "personal-projects", title: "Personal Projects", folder: "dataviz/personal-projects", cover: "01-nightingale-redo.png", description: "These are the dataviz projects that I take on just for fun!" },
+            { slug: "grad-school-projects", title: "Grad School Projects", folder: "dataviz/grad-school-projects", cover: "01-geologic-map.png", description: "This is a selection of visuals that I made during my PhD—figures for papers, figures for communication to the public, sketches & illustrations, etc." },
+            { slug: "sports-viz-sunday", title: "Sports Viz Sunday", folder: "dataviz/sports-viz-sunday", cover: "01-wheelchair-marathon.png", description: "These data visualizations were made as part of Sports Viz Sunday, a monthly data visualization challenge." },
         ],
     },
     {
         supercategoryTitle: "Art",
         categories: [
-            { slug: "conference-sketchnotes", title: "Conference Sketchnotes", folder: "art/conference-sketchnotes", cover: "01-mu69-session.png" },
-            { slug: "apollo-11-50th-anniversary", title: "Apollo 11 50th Anniversary", folder: "art/apollo-11-50th-anniversary", cover: "01-apollo-head-image.png" },
-            { slug: "pen-plotter", title: "Pen Plotter", folder: "art/pen-plotter", cover: "01-raspberry-lemonade.png" },
-            { slug: "letter-the-moons", title: "Letter the Moons", folder: "art/letter-the-moons", cover: "01-neptune-irregulars.png" },
-            { slug: "lpsc-session-titles", title: "LPSC Session Titles", folder: "art/lpsc-session-titles", cover: "01-lpsc-exoplanets.png" },
-            { slug: "inktober-2019", title: "Inktober 2019", folder: "art/inktober-2019", cover: "01-inktober.png" },
-            { slug: "brush-pens", title: "Brush Pens", folder: "art/brush-pens", cover: "01-brush-pens.png" },
+            { slug: "conference-sketchnotes", title: "Conference Sketchnotes", folder: "art/conference-sketchnotes", cover: "01-mu69-session.png", description: "At conferences during grad school I loved taking 'sketchnotes', or notes filled out with sketches and diagrams related to the speaker's presentation. These sketchnotes were taken during the Lunar and Planetary Science Conference, my go-to conference during graduate school." },
+            { slug: "apollo-11-50th-anniversary", title: "Apollo 11 50th Anniversary", folder: "art/apollo-11-50th-anniversary", cover: "01-apollo-head-image.png", description: "In July 2019, I took part in an Instagram lettering challenge hosted in honor of the 50th anniversary of the Apollo 11 mission to the Moon. I had a great time with the prompts and the first image on this page, a quote from astronaut Gene Cernan, is one of my favorite lettering pieces that I've ever made." },
+            { slug: "pen-plotter", title: "Pen Plotter", folder: "art/pen-plotter", cover: "01-raspberry-lemonade.png", description: "After years of wishing for one, in 2022 I finally got a pen plotter! It's an AxiDraw V3 and has a plotting area of 8.5” x 11”. I've had such a fun time playing with form and color, and listening to the little robot noises it makes when it's plotting." },
+            { slug: "letter-the-moons", title: "Letter the Moons", folder: "art/letter-the-moons", cover: "01-neptune-irregulars.png", description: "In 2018 I made it a personal challenge to letter each of the (at the time) 147 named moons in the solar system, which I completed over five months. These images are a selection of my pieces for this challenge." },
+            { slug: "lpsc-session-titles", title: "LPSC Session Titles", folder: "art/lpsc-session-titles", cover: "01-lpsc-exoplanets.png", description: "I've always gotten a kick out of the jokey session titles at the Lunar and Planetary Science Conference, so in 2020 I took it upon myself to make some title cards for my favorite session names." },
+            { slug: "inktober-2019", title: "Inktober 2019", folder: "art/inktober-2019", cover: "01-inktober.png", description: "In 2019 I participated in a planetary-science-specific version of Inktober on Twitter. All my pieces were made digitally, but I think it still counts for 'ink'tober!" },
+            { slug: "brush-pens", title: "Brush Pens", folder: "art/brush-pens", cover: "01-brush-pens.png", description: "Not all my artwork has a digital component! This series was me going back to basics and practicing my brush pen calligraphy." },
         ],
     },
 ];
