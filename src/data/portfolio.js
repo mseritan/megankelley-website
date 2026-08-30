a832f040450fb21c33157c6457655c31a4165303
Megan Seritan <meganseritan@Megans-MacBook-Air.local>
2026-08-29 21:34:12 -0700

    Add portfolio tab and grid page

---
+++ src/data/portfolio.js
@@ -0,0 +1,22 @@
+export const portfolio = [
+    {
+        supercategoryTitle: "Dataviz",
+        categories: [
+            { slug: "personal-projects", title: "Personal Projects", folder: "dataviz/personal-projects", cover: "01-nightingale-redo.png" },
+            { slug: "grad-school-projects", title: "Grad School Projects", folder: "dataviz/grad-school-projects", cover: "01-geologic-map.png" },
+            { slug: "sports-viz-sunday", title: "Sports Viz Sunday", folder: "dataviz/sports-viz-sunday", cover: "01-wheelchair-marathon.png" },
+        ],
+    },
+    {
+        supercategoryTitle: "Art",
+        categories: [
+            { slug: "conference-sketchnotes", title: "Conference Sketchnotes", folder: "art/conference-sketchnotes", cover: "01-mu69-session.png" },
+            { slug: "apollo-11-50th-anniversary", title: "Apollo 11 50th Anniversary", folder: "art/apollo-11-50th-anniversary", cover: "01-apollo-head-image.png" },
+            { slug: "pen-plotter", title: "Pen Plotter", folder: "art/pen-plotter", cover: "01-raspberry-lemonade.png" },
+            { slug: "letter-the-moons", title: "Letter the Moons", folder: "art/letter-the-moons", cover: "01-neptune-irregulars.png" },
+            { slug: "lpsc-session-titles", title: "LPSC Session Titles", folder: "art/lpsc-session-titles", cover: "01-lpsc-exoplanets.png" },
+            { slug: "inktober-2019", title: "Inktober 2019", folder: "art/inktober-2019", cover: "01-inktober.png" },
+            { slug: "brush-pens", title: "Brush Pens", folder: "art/brush-pens", cover: "01-brush-pens.png" },
+        ],
+    },
+];
